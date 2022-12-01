6cb6a217a8b223cada93c1859cc47b56d0fd204c
Johnny Wright <johnnywrightiv@gmail.com>
2022-12-01 13:46:48 -0600

    Add more dynamic elements and interactivity

--- scripts/album.js
+++ scripts/album.js
@@ -1,16 +1,16 @@
 var createSongRow = function (songNumber, songName, songLength) {
-    if (songLength >= 60) {
-      // var hours = Math.floor(songLength / 3600);
-      // to add hours, uncomment hours line and change minutes to = < songLength - hours * 60 >
-      var minutes = Math.floor(songLength / 60);
-      var seconds = songLength - minutes * 60;
-      
-      if (seconds < 10) {
-        seconds = `0${seconds}`
-      };
+  if (songLength >= 60) {
+    // var hours = Math.floor(songLength / 3600);
+    // to add hours, uncomment hours line and change minutes to = < songLength - hours * 60 >
+    var minutes = Math.floor(songLength / 60);
+    var seconds = songLength - minutes * 60;
+    
+    if (seconds < 10) {
+      seconds = `0${seconds}`
+    };
 
-      songLength = `${minutes}:` + `${seconds}`
-    }
+    songLength = `${minutes}:` + `${seconds}`
+  }
 
   var template =
     '<tr class="album-view-song-item">'
@@ -40,19 +40,21 @@ var createSongRow = function (songNumber, songName, songLength) {
       currentSoundFile.play();
 
       $(this).html(pauseButtonTemplate);
-  
-    // 3. The currently playing song was clicked
+      $mainPlayButton.html(pauseButtonTemplate);
+      
+      // 3. The currently playing song was clicked
     } else {
       if (currentSoundFile.isPaused()) {
         currentSoundFile.play();
         $(this).html(pauseButtonTemplate);
+        $mainPlayButton.html(pauseButtonTemplate);
       } else {
         currentSoundFile.pause();
         $(this).html(playButtonTemplate);
+        $mainPlayButton.html(playButtonTemplate);
       }
     }
   };
-
 
 
   var onHover = function () {
@@ -113,40 +115,54 @@ var setCurrentAlbum = function(album) {
 
   }
 
-var handlePlayPauseClick = function () {
-  if (currentSoundFile.isPaused()) {
-    currentSoundFile.play();
-    $(this).html(pauseButtonTemplate);
-  } else {
-    currentSoundFile.pause();
-    $(this).html(playButtonTemplate);
+  var handlePlayPauseClick = function () {
+    if (currentlyPlayingSongNumber === null) {
+      currentlyPlayingSongNumber = 1;
+      setSong(currentlyPlayingSongNumber);
+    };
+
+    if (currentSoundFile.isPaused()) {
+      currentSoundFile.play();
+      $(this).html(pauseButtonTemplate);
+    } else {
+      currentSoundFile.pause();
+      $(this).html(playButtonTemplate);
+    };
   };
-};
 
-var handlePrevClick = function () {
-  // work on song progress logic first... if current song progress < 3 seconds, repeat current song from begining, else play previous song
-  if (songIndex > 0) {
-    songIndex --;
-  }
-  currentSoundFile.stop()
-  currentSoundFile.play()
-};
+  var handlePrevClick = function () {
+    setSong(currentlyPlayingSongNumber);
+    
+    playbackTime = currentSoundFile.getTime();
+    
+    if (playbackTime < 0) { // change 0 to 2 or 3 seconds to make restart/previous functionality work... need to use .getTime() to access current play length... likely have to make progress bar work before I can make this functionality work. For now, with 0 in place, it just functions as a previous button (else) and ignores the restart function (if).
+      currentlyPlayingSongNumber --;
+      setSong(currentlyPlayingSongNumber);
+      currentSoundFile.stop();
+      currentSoundFile.play();
+    } else {
+      currentSoundFile.stop();
+      currentSoundFile.play();
+    };
+  };
 
-var handleNextClick = function () {
-  currentSoundFile.stop()
-  songIndex ++;
-  currentSoundFile.play()
-};
+  var handleNextClick = function () {
+    currentlyPlayingSongNumber ++;
+    currentSoundFile.stop();
+    setSong(currentlyPlayingSongNumber);
+    currentSoundFile.play()
+  };
 
-$mainPlayButton.click(handlePlayPauseClick)
-$mainPrevButton.click(handlePrevClick)
-$mainNextButton.click(handleNextClick)
-
+  $mainPlayButton.click(handlePlayPauseClick)
+  $mainPrevButton.click(handlePrevClick)
+  $mainNextButton.click(handleNextClick)
+  $volumeSlider.click(getVolumeVal);
 };
 
 var setSong = function (songNumber) {
   var $displayCurrentSong = $('.song-name');
   var $displayCurrentArtist = $('.artist-name');
+  var $displaySongDuration = $('.total-time')
   if (currentSoundFile) {
     currentSoundFile.stop();
   }
@@ -160,12 +176,28 @@ var setSong = function (songNumber) {
 
   $displayCurrentSong.html(currentAlbum.songs[currentlyPlayingSongNumber - 1].title);
   $displayCurrentArtist.html(currentAlbum.artist);
+
+  songDuration = currentAlbum.songs[currentlyPlayingSongNumber - 1].duration;
+  if (songDuration >= 60) {
+    var minutes = Math.floor(songDuration / 60);
+    var seconds = songDuration - minutes * 60;
+    
+    if (seconds < 10) {
+      seconds = `0${seconds}`
+    };
+    
+    songDuration = `${minutes}:` + `${seconds}`
+  }
+  $displaySongDuration.html(songDuration);
+
 };
 
 var songIndex = 0;
 var $mainPlayButton = $('.play-pause')
 var $mainNextButton = $('.next')
 var $mainPrevButton = $('.previous')
+var $volumeSlider = $('#vol-ctrl')
+var $currentTime = $('.current-time')
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 
@@ -174,3 +206,26 @@ var currentSoundFile = null;
 var currentAlbum = null;
 
 setCurrentAlbum(albums[0]);
+
+var getVolumeVal = function() {
+  volume = $volumeSlider.val();
+  currentSoundFile.setVolume(volume);
+};
+
+// Track song progress
+// setInterval(() => {
+//     playTimeElapsed = currentSoundFile.getTime()
+    
+//     if (playTimeElapsed >= 60) {
+//       var minutes = Math.floor(playTimeElapsed / 60);
+//       var seconds = playTimeElapsed - minutes * 60;
+      
+//       if (seconds < 10) {
+//         seconds = `0${seconds}`
+//       };
+      
+//       songDuration = `${minutes}:` + `${seconds}`
+//       console.log(songDuration)
+//     }
+//     $currentTime.html('work plz')
+// }, 1000);
